0feba6a39d5a68cb2d24093f53b89be78dcf34e9
marinhomateus <mateus.marinho@hotmail.com.br>
2022-06-09 13:33:44 +0000

    upload packs

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - nuxtapp',
-    title: 'nuxtapp',
+    // titleTemplate: '%s - app',
+    title: 'App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
