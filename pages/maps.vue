ce99013bc325314f4f2855f536d5b3fa08796056
marinhomateus <mateus.marinho@hotmail.com.br>
2022-06-10 11:26:30 +0000

    :wrench: fix (maps)

--- pages/maps.vue
+++ pages/maps.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-row>
-    <v-col cols="12">
+  <v-row class="ma-2">
+    <v-col cols="12" md="6">
       <v-card-text class="px-0 pb-0">
         <v-sheet>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26356315.701909266!2d-113.65984645545673!3d36.25591957613731!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sus!4v1566158729223!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4218.19014244188!2d-50.96751848434606!3d-29.93959393336933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95190badd1de9071%3A0x387e9462bd1a554e!2sPrometeon!5e1!3m2!1spt-BR!2sbr!4v1654859939687!5m2!1spt-BR!2sbr"
             width="100%"
             height="400"
             frameborder="0"
@@ -19,7 +19,39 @@
       <v-card-text class="px-0 pb-0">
         <v-sheet>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15831228695!2d-74.11976206978034!3d40.697663747508045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY!5e0!3m2!1sen!2sus!4v1566158235149!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4458.555368987018!2d-46.5054425844738!3d-23.661736271193046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce69ce5f108d25%3A0x2ea39f81f203909f!2sPrometeon%20Tyre%20Group%20Industria%20Brasil%20Ltda.!5e1!3m2!1spt-BR!2sbr!4v1654859825423!5m2!1spt-BR!2sbr"
+            width="100%"
+            height="400"
+            style="border: 0"
+            allowfullscreen
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </v-sheet>
+      </v-card-text>
+    </v-col>
+
+    <v-col cols="12" md="12">
+      <v-card-text class="px-0 pb-0">
+        <v-sheet>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2211.5570632122444!2d9.211909652285767!3d45.51961975158257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786c723bc228e89%3A0x7add146b46a529e1!2sPrometeon%20Tyre%20Group!5e1!3m2!1spt-BR!2sbr!4v1654860270544!5m2!1spt-BR!2sbr"
+            width="100%"
+            height="400"
+            style="border: 0"
+            allowfullscreen
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </v-sheet>
+      </v-card-text>
+    </v-col>
+
+    <v-col cols="12" md="6">
+      <v-card-text class="px-0 pb-0">
+        <v-sheet>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4621.893562771597!2d29.845169532461618!3d30.995400826139267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f5eda2e6599681%3A0xb373c93eb06f04f5!2sPirelli%20Egypt!5e1!3m2!1spt-BR!2sbr!4v1654859569152!5m2!1spt-BR!2sbr"
             width="100%"
             height="400"
             frameborder="0"
@@ -34,7 +66,7 @@
       <v-card-text class="px-0 pb-0">
         <v-sheet>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15831228695!2d-74.11976206978034!3d40.697663747508045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY!5e0!3m2!1sen!2sus!4v1566158235149!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3476.4760524854387!2d29.999948401478527!3d40.753770167182886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cb506d08f58d15%3A0xb5681f11f60de2dc!2sPrometeon%20Turkey%20End%C3%BCstriyel%20ve%20Ticari%20Lastikler%20A.%C5%9E.!5e1!3m2!1spt-BR!2sbr!4v1654860019699!5m2!1spt-BR!2sbr"
             width="100%"
             height="400"
             frameborder="0"
