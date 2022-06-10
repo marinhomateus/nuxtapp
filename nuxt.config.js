f49e1721c48c2a7a7b9a617c5c4a2b18dd724b0f
marinhomateus <mateus.marinho@hotmail.com.br>
2022-06-10 10:57:09 +0000

    :wrench: fix (light theme default)

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,7 @@ export default {
       options: {
         customProperties: true
       },
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           prometeon: "#212b59",
